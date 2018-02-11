74dd57b2f327ed4ef8f701b5123d975a6acd3603
dmannock <dmannock@users.noreply.github.com>
2018-02-11 10:26:13 +0000

    extracted functions to own file

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,16 @@
 'use strict';
 import * as vscode from 'vscode';
-
-type SelectionDetails = {
-    text: string,
-    range: vscode.Range,
-    line: number,
-    selection: vscode.Selection    
-}
-
-type RangeProvider = (sel: vscode.Selection) => vscode.Range
+import { 
+    bindingLineRegex,
+    getSelectionDetails, 
+    isSelectionValid,
+    getIndentation, 
+    getExpandedSelection,
+    getBindingDeclarationAbove,
+    getWordInstancesBelow } from './core'
 
 export function activate(context: vscode.ExtensionContext) {
 
-    const bindingLineRegex = /^(\s+)(let)\s+(\S+)\s+=\s+([\s\S]+)/;
-    
     console.log("F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#");
     console.log("F# Fsharp Refactor extension active :)  F#");
     console.log("F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#");
@@ -41,14 +38,6 @@ export function activate(context: vscode.ExtensionContext) {
     }));
     
     context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.inlineLet", (editor) => {
-        //(*) get cursor pos
-        //(*) expand to whole word
-        //(*) is word @ let binding?
-        //(*) -> find all instances below (within context)
-        //(*)    that have the sam eindentation or more
-        //(*) -> else it is one of the instances
-        //(*)   1) find above with let binding (naive) s could go out of context 
-        //(*)   2) find other instances below if any
         //TODO: refactor!
         const { document, selections } = editor;
         if (document.languageId != "fsharp") {
@@ -89,88 +78,6 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
     }));
-
-    function isSelectionValid(selectionDetails: SelectionDetails) {
-        if (!selectionDetails.range.isSingleLine) {
-            return vscode.window.showWarningMessage("Multiple line selections are not supported");
-        }
-        if (selectionDetails.range.isEmpty) {
-            return vscode.window.showInformationMessage("Select the expression to extract");
-        }
-        return true;
-    }
-    
-    function getExpandedSelection(sel: vscode.Selection[], doc: vscode.TextDocument): SelectionDetails {
-        const selection = sel[0];
-        const wordRange = doc.getWordRangeAtPosition(new vscode.Position(sel[0].start.line, sel[0].start.character))
-        const text = doc.getText(wordRange);
-        return {
-            text: text,
-            range: wordRange,
-            line: wordRange.start.line,
-            selection: selection
-        }
-    }
-
-    function getSelectionDetails(sel: vscode.Selection[], doc: vscode.TextDocument): SelectionDetails {
-        const selection = sel[0];
-        const wordRange = new vscode.Range(selection.start, selection.end);
-        const text = doc.getText(wordRange);
-        return {
-            text: text,
-            range: wordRange,
-            line: wordRange.start.line,
-            selection: selection
-        }
-    }
-
-    function getIndentation(doc: vscode.TextDocument, line: number): string {
-        const matched = doc.lineAt(line).text.match(/^\s+/);
-
-        return matched ? matched[0] : "";
-    }
-
-    function wordIndexesInText(text: string, toFind: string): number[] {
-        const found = [];
-        let lastWordIndex = -1;
-        while (true) {
-            lastWordIndex = text.indexOf(toFind, lastWordIndex + 1);
-            if (lastWordIndex == -1) {
-                break;
-            }
-            found.push(lastWordIndex);
-        }
-        return found;
-    }
-
-    function getWordInstancesBelow(doc: vscode.TextDocument, word: string, startingLine: number, indentationCharCount: number): vscode.Position[] {
-        const positions = [];
-        for (let i = startingLine; i < doc.lineCount; i++) {
-            const currentLine = doc.lineAt(i);
-            if (currentLine.firstNonWhitespaceCharacterIndex < indentationCharCount) {
-                break;
-            }
-            wordIndexesInText(currentLine.text, word)
-                .map(wordIndex => doc.getWordRangeAtPosition(new vscode.Position(i, wordIndex)))
-                .forEach(position => positions.push(position));
-        }
-        return positions;
-    }
-
-    function getBindingDeclarationAbove(doc: vscode.TextDocument, word: string, startingLine: number, indentationCharCount: number) {
-        let currentLine: vscode.TextLine;
-        for (let i = startingLine; i >= 0; i--) {
-            currentLine = doc.lineAt(i);
-            if (currentLine.firstNonWhitespaceCharacterIndex > currentLine.firstNonWhitespaceCharacterIndex) {
-                break;
-            }
-            return {
-                matchedBindingLine: currentLine.text.match(bindingLineRegex),
-                matchedLine: currentLine
-            };
-        }
-        return null;
-    }
 }
 
 export function deactivate() {
