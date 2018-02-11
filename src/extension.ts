f26aadc67b6e95a24204d4c8a4ab84cec2705a01
dmannock <dmannock@users.noreply.github.com>
2018-02-11 10:43:52 +0000

    tslint & linted

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,26 @@
-'use strict';
-import * as vscode from 'vscode';
-import { 
+"use strict";
+import * as vscode from "vscode";
+import {
     bindingLineRegex,
-    getSelectionDetails, 
-    isSelectionValid,
-    getIndentation, 
-    getExpandedSelection,
     getBindingDeclarationAbove,
-    getWordInstancesBelow } from './core'
+    getExpandedSelection,
+    getIndentation,
+    getSelectionDetails,
+    getWordInstancesBelow,
+    isSelectionValid,
+} from "./core";
 
 export function activate(context: vscode.ExtensionContext) {
 
+    /* tslint:disable:no-console */
     console.log("F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#");
     console.log("F# Fsharp Refactor extension active :)  F#");
     console.log("F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#F#");
-    
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand('extension.extractLet', (editor) => {
+    /* tslint:enable:no-console */
+
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.extractLet", (editor) => {
         const { document, selections } = editor;
-        if (document.languageId != "fsharp") {
+        if (document.languageId !== "fsharp") {
             return;
         }
         if (selections.length > 1) {
@@ -29,18 +32,18 @@ export function activate(context: vscode.ExtensionContext) {
         }
         const initialBindingName = "extracted";
         const indentation = getIndentation(document, selectionDetails.line);
-        editor.edit(eb => {
+        editor.edit((eb) => {
             eb.replace(selectionDetails.selection, initialBindingName);
-            eb.insert(new vscode.Position(selectionDetails.line, 0), 
-            `${indentation}let ${initialBindingName} = ${selectionDetails.text}\r\n`);
+            eb.insert(new vscode.Position(selectionDetails.line, 0),
+                `${indentation}let ${initialBindingName} = ${selectionDetails.text}\r\n`);
         });
-        vscode.commands.executeCommand("editor.action.rename")
+        vscode.commands.executeCommand("editor.action.rename");
     }));
-    
+
     context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.inlineLet", (editor) => {
-        //TODO: refactor!
+        // TODO: refactor! (such nesting, many brackets, uggh!)
         const { document, selections } = editor;
-        if (document.languageId != "fsharp") {
+        if (document.languageId !== "fsharp") {
             return;
         }
         if (selections.length > 1) {
@@ -51,34 +54,44 @@ export function activate(context: vscode.ExtensionContext) {
             return;
         }
         const currentLine = document.lineAt(selectionDetails.line);
-        const matchedBindingLine = currentLine.text.match(bindingLineRegex)
+        const matchedBindingLine = currentLine.text.match(bindingLineRegex);
         if (matchedBindingLine) {
             const [, indentation, binding, bindingName, expression] = matchedBindingLine;
-            const occurancesToReplace = getWordInstancesBelow(document, bindingName, selectionDetails.line + 1, currentLine.firstNonWhitespaceCharacterIndex);
-            editor.edit(eb => {
+            const occurancesToReplace = getWordInstancesBelow(
+                document,
+                bindingName,
+                selectionDetails.line + 1,
+                currentLine.firstNonWhitespaceCharacterIndex);
+
+            editor.edit((eb) => {
                 eb.delete(currentLine.rangeIncludingLineBreak);
                 for (const range of occurancesToReplace) {
                     eb.replace(range, expression);
                 }
             });
         } else {
-            //naive initial concept
-            const matchedBindingLine = getBindingDeclarationAbove(document, selectionDetails.text, selectionDetails.line - 1, currentLine.firstNonWhitespaceCharacterIndex);
-            if (matchedBindingLine) {
-                const [, indentation, binding, bindingName, expression] = matchedBindingLine.matchedBindingLine;
-                const occurancesToReplace = getWordInstancesBelow(document, bindingName, matchedBindingLine.matchedLine.lineNumber + 1, matchedBindingLine.matchedLine.firstNonWhitespaceCharacterIndex);
-                editor.edit(eb => {
-                    eb.delete(matchedBindingLine.matchedLine.rangeIncludingLineBreak);
+            // naive initial concept
+            const matchedBindingLineAbove = getBindingDeclarationAbove(document,
+                selectionDetails.text,
+                selectionDetails.line - 1,
+                currentLine.firstNonWhitespaceCharacterIndex);
+
+            if (matchedBindingLineAbove) {
+                const [, indentation, binding, bindingName, expression] = matchedBindingLineAbove.matchedBindingLine;
+                const occurancesToReplace = getWordInstancesBelow(document,
+                    bindingName,
+                    matchedBindingLineAbove.matchedLine.lineNumber + 1,
+                    matchedBindingLineAbove.matchedLine.firstNonWhitespaceCharacterIndex);
+
+                editor.edit((eb) => {
+                    eb.delete(matchedBindingLineAbove.matchedLine.rangeIncludingLineBreak);
                     for (const range of occurancesToReplace) {
-                        //wrapped in brackets to ensure precidence is preserved (without knowing usage context)
+                        // wrapped in brackets to ensure precidence is preserved (without knowing usage context)
                         eb.replace(range, `(${expression})`);
                     }
-                });        
+                });
             }
         }
 
     }));
-}
-
-export function deactivate() {
 }
